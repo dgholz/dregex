6de712274ad6161f8a38886bef7ab5eed7011073
Daniel Holz <dgholz@gmail.com>
2022-10-27 15:46:37 +0100

    Track which states can be reached without consuming input

--- lib/dregex/state_machine/nfa.rb
+++ lib/dregex/state_machine/nfa.rb
@@ -1,7 +1,33 @@
+require 'set'
+
 require 'dregex/state_machine/dfa'
 
 module Dregex
   class StateMachine
-    class NFA < FiniteAutomata; end
+    class NFA < FiniteAutomata
+
+      def epsilon_transitions
+        ets = states.keys.map do |state_name|
+          e_transition = states[state_name][:empty]
+          [
+            state_name,
+            Set.new([state_name, e_transition].compact)
+          ]
+        end.to_h
+
+        ets.each do |state_name, e_reachable_states|
+          seen = [ state_name ].to_set
+          to_visit = e_reachable_states.to_a
+          while ! to_visit.empty?
+            look_at = to_visit.shift
+            seen.add look_at
+            ets[state_name].add look_at
+            to_visit.push *(ets[look_at] - seen).to_a
+          end
+        end
+        ets
+      end
+
+    end
   end
 end
