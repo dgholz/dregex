7846269104b31dbc8f56c8eb5ad4f7af7fb39ae6
Daniel Holz <dgholz@gmail.com>
2022-10-27 15:46:37 +0100

    Track which states can be reached without consuming input

--- spec/dregex/state_machine/nfa_spec.rb
+++ spec/dregex/state_machine/nfa_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require "set"
 
 describe Dregex::StateMachine::NFA do
   let(:nfa) { Dregex::StateMachine::NFA.new }
@@ -49,5 +50,26 @@ describe Dregex::StateMachine::NFA do
     state2[:end] = true
 
     expect(nfa.to_state_machine).must_equal state0
+  end
+
+  describe "#epsilon_transitions" do
+    let(:tokeniser) { Dregex::Tokeniser.new(pattern) }
+    let(:pattern) { "foo" }
+    let(:parser) { Dregex::Parser.new(tokeniser) }
+    let(:builder) { Dregex::StateMachine::Builder.new }
+    let(:nfa) { builder.build_from parser.ast; builder.nfa }
+    let(:pattern) { "fo*ot+" }
+    it "tracks which states can be reached via epsilon transitions" do
+      expect(nfa.epsilon_transitions).must_equal({
+        S0: Set.new([:S0]),
+        S1: Set.new([:S1, :S3]),
+        S2: Set.new([:S1, :S2, :S3]),
+        S3: Set.new([:S3]),
+        S4: Set.new([:S4]),
+        S5: Set.new([:S5, :S7]),
+        S6: Set.new([:S5, :S6, :S7]),
+        S7: Set.new([:S7]),
+      })
+    end
   end
 end
